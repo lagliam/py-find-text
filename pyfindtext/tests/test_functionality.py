601ba327b705cb35fe70d6b36816099ecdc1cf62
lagliam <liam@goring.ca>
2019-08-05 16:39:17 -0500

    Initial Commit

---
+++ pyfindtext/tests/test_functionality.py
@@ -0,0 +1,20 @@
+import pyfindtext
+import os
+
+test_dir = os.path.abspath('test_files')
+
+
+def test_functionality_1():
+    x = pyfindtext.find(test_dir, 'sbrannigan')
+    # expect line number to be 3
+    assert x[0][1] == 3
+    assert x[0][2] == 'Steve,Brannigan,sbrannigan,changeme,sbrannigan@kanab.' \
+                   'org,123-456-7890,3,1,1041,Teacher,328 Innovation,Suite ' \
+                   '# 200 ,state college,PA,16803\r\n'
+
+
+def test_functionality_2():
+    y = pyfindtext.find(test_dir, 'CIRCUMFLEX')
+    # expect data returned equals expected result from file
+    assert y[0][2] == '5E  CIRCUMFLEX ACCENT           ' \
+                   'DE  CAPITAL LETTER THORN (Icelandic)\n'
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyfindtext',
+    version='1.0.0',
+    packages=['pyfindtext'],
+    url='https://github.com/lagliam/py-find-text',
+    license='GNU General Public License v3.0',
+    author='Liam Goring',
+    author_email='liam@liamgoring.com',
+    description='Find text in files'
+)
